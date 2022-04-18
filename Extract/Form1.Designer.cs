1fd6cb40d1dca0a41196e9fdbb04fc3c378c6b31
Java Software <pj.midgard@yahoo.com>
2022-04-18 12:37:58 +0100

    Add files via upload

---
+++ Extract/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace howto_use_dotnetzip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnExtractArchive = new System.Windows.Forms.Button();
+            this.txtExtractTo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblCompressedSize = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArchiveName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btnExtractArchive);
+            this.groupBox3.Controls.Add(this.txtExtractTo);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(15, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(450, 75);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Extract To";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // btnExtractArchive
+            // 
+            this.btnExtractArchive.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnExtractArchive.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtractArchive.Location = new System.Drawing.Point(175, 45);
+            this.btnExtractArchive.Name = "btnExtractArchive";
+            this.btnExtractArchive.Size = new System.Drawing.Size(95, 23);
+            this.btnExtractArchive.TabIndex = 1;
+            this.btnExtractArchive.Text = "Extract ";
+            this.btnExtractArchive.UseVisualStyleBackColor = true;
+            this.btnExtractArchive.Click += new System.EventHandler(this.btnExtractArchive_Click);
+            // 
+            // txtExtractTo
+            // 
+            this.txtExtractTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtExtractTo.Location = new System.Drawing.Point(107, 19);
+            this.txtExtractTo.Name = "txtExtractTo";
+            this.txtExtractTo.Size = new System.Drawing.Size(334, 20);
+            this.txtExtractTo.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Path:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblCompressedSize
+            // 
+            this.lblCompressedSize.AutoSize = true;
+            this.lblCompressedSize.Location = new System.Drawing.Point(107, 42);
+            this.lblCompressedSize.Name = "lblCompressedSize";
+            this.lblCompressedSize.Size = new System.Drawing.Size(0, 13);
+            this.lblCompressedSize.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Size:";
+            // 
+            // txtArchiveName
+            // 
+            this.txtArchiveName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArchiveName.Location = new System.Drawing.Point(82, 22);
+            this.txtArchiveName.Name = "txtArchiveName";
+            this.txtArchiveName.Size = new System.Drawing.Size(356, 20);
+            this.txtArchiveName.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtArchiveName);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.lblCompressedSize);
+            this.groupBox2.Location = new System.Drawing.Point(15, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(472, 67);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 181);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Dzordz 1.0.0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtExtractTo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnExtractArchive;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblCompressedSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArchiveName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Extract/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// Download and unzip the DotNetZip library at: http://dotnetzip.codeplex.com/
+// Add a reference to the Ionic.Zip.dll library.
+
+using System.IO;
+using Ionic.Zip;
+
+namespace howto_use_dotnetzip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Start with an initial file.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            txtExtractTo.Text = Path.GetFullPath(
+                Path.Combine(Application.StartupPath, "..\\..\\ExtractedFiles\\"));
+        }
+
+        
+
+        // Extract the files from the archive.
+        private void btnExtractArchive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ZipFile zip = ZipFile.Read(txtArchiveName.Text))
+                {
+                    // Loop through the archive's files.
+                    foreach (ZipEntry zip_entry in zip)
+                    {
+                        zip_entry.Extract(txtExtractTo.Text);
+                    }
+                }
+
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error extracting archive.\n" + ex.Message);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
